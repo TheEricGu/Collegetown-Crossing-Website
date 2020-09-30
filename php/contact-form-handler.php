1e44bf66b6224c963f0967e7c60ffd889cf01b4d
Eric Gu <efg37@cornell.edu>
2020-09-30 02:30:51 -0400

    Completed implementation of Josh's requests and JS effects

--- php/contact-form-handler.php
+++ php/contact-form-handler.php
@@ -1,6 +1,7 @@
 <?php
-$name = %_POST['name'];
+$name = $_POST['name'];
 $visitor_email = $_POST['email'];
+$phone_number = $_POST['number'];
 $message = $_POST['message']
 
 $email_from = 'ericgu51893@gmail.com';
@@ -9,6 +10,7 @@ $email_subject = "New Form Submission";
 
 $email_body = "User Name: $name.\n".
 "User Email: $visitor_email.\n".
+"User Phone Number: $phone_number.\n".
 "User Message: $message.\n";
 
 $to = "ericg51893@gmail.com";
@@ -19,5 +21,5 @@ $headers .= "Reply-To: $visitor_email \r\n";
 
 nail($to,$email_subject,$email_body,$headers);
 
-header("Location: index.html");
+header("Location: ../index.html");
 ?>
