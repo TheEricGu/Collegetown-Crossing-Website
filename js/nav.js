55532f7c6ac83e7084e857a1c97b1d0ee5fb4a1f
Eric Gu <efg37@cornell.edu>
2020-10-01 03:27:59 -0400

    Added responsive features to Home page

---
+++ js/nav.js
@@ -0,0 +1,16 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+
+  navbarLinks.forEach((link, index) => {
+    if (link.style.animation) {
+      link.style.animation = '';
+    } else {
+      link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s`;
+    }
+  });
+
+})
+
